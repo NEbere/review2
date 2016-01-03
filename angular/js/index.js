bbe21a77eb37079604bed583823764ec39039c13
NEbere <nwosu.happiness2012@gmail.com>
2016-01-03 04:44:19 +0100

    Review for software developer, Javascript

---
+++ angular/js/index.js
@@ -0,0 +1,37 @@
+var db = new PouchDB('angulardb');
+db.info().then(function (info) {
+  //document.getElementById('display').innerHTML = 'We have a database: ' + JSON.stringify(info);
+});
+
+
+var doc = {
+  "_id": "mittens",
+  "name": "Mittens",
+  "occupation": "kitten",
+  "age": 3,
+  "hobbies": [
+    "playing with balls of yarn",
+    "chasing laser pointers",
+    "lookin' hella cute"
+  ]
+};
+
+// Destroy the database before doing anything, because I want 
+// you to see the same thing if you reload.
+// Ignore the man behind the curtain!
+
+new PouchDB('sample').destroy().then(function () {
+  return new PouchDB('sample');
+}).then(function (db) {
+  //
+  // IMPORTANT CODE STARTS HERE
+  //
+  db.put(doc).then(function () {
+    return db.get('mittens');
+  }).then(function (doc) {
+    document.getElementById('display').innerHTML = 'We stored a kitten: ' + JSON.stringify(doc);
+  });
+  //
+  // IMPORTANT CODE ENDS HERE
+  //
+});
